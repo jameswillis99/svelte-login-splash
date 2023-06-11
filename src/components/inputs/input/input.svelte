182c399f6c5d133f986716c01c8f13b7b2db63e7
James Willis <jw@eliasam.com>
2023-06-11 15:04:26 +0100

    feat: add email and password input box and page style

---
+++ src/components/inputs/input/input.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let value = "";
+  export let label = "";
+</script>
+
+<div class="inputContainer">
+  <label for="input">{label}:</label>
+  <input id="input" bind:value />
+</div>
+
+<style>
+  .inputContainer {
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    background-color: transparent;
+    color: white;
+  }
+  label {
+    display: inline-block;
+    width: 100px;
+    text-align: right;
+  }
+  input {
+    background-color: transparent;
+    color: white;
+    width: 150px;
+    border: none;
+    border-bottom: 1px solid rgb(182, 182, 182);
+    padding: 5px;
+  }
+  input:focus {
+    outline: none;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Input from "../components/inputs/input/input.svelte";
+
+  let emailValue = "";
+  let passwordValue = "";
+</script>
+
+<div class="content">
+  <div class="item">
+    <h1>Welcome</h1>
+    <h5>Please sign in below.</h5>
+  </div>
+  <!-- <p>{inputValue}</p> -->
+  <div class="item">
+    <Input bind:value={emailValue} label={'Email'}/>
+  </div>
+  <div class="item">
+    <Input bind:value={passwordValue} label={'Password'}/>
+  </div>
+</div>
+
+<style>
+
+  .content {
+    padding-top: 100px;
+    margin: 0;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    color: white;
+  }
+
+  .item {
+    flex-basis: 100%;
+    text-align: center;
+  }
+</style>
